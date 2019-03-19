46b524bafe9b1d2853a2949ac4be075796a5c0c2
Christopher Beckford <christopher.beckford@foundry.com>
2019-03-19 00:14:52 +0000

    Adding cppzmq

--- cmake/projects/cppzmq/hunter.cmake
+++ cmake/projects/cppzmq/hunter.cmake
@@ -17,6 +17,17 @@ hunter_add_version(
   SHA1
   afc87286b15b035dbabcb99ef410143c3a24685c
   )
+  
+hunter_add_version(
+  PACKAGE_NAME
+  cppzmq
+  VERSION
+  "4.3.0-91fd0b5"
+  URL
+  "https://github.com/zeromq/cppzmq/archive/91fd0b5e0e61b078018f1b81c9c03f03e5de5c3f.tar.gz"
+  SHA1
+  c757d98054e47de672b777d8d108b71abeefb11c
+  )
 
 hunter_cmake_args(cppzmq CMAKE_ARGS ZMQ_BUILD_TESTS=OFF)
 hunter_pick_scheme(DEFAULT url_sha1_cmake) 
